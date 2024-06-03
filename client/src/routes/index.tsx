3b20954925457a37c93a78578878175a5060c7e8
José Victor <jose08.victor@gmail.com>
2024-06-03 19:33:20 -0300

    feat: footer and banners added.

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -3,8 +3,12 @@ import { NavBar } from '../components/navbar'
 import { Shop } from '../pages/Shop'
 import { Cart } from '../pages/Cart'
 import { Product } from '../pages/Product'
-import { ShopCategory } from '../pages/ShopCategory'
+import { ShopCategory } from '../pages/shopCategory'
 import { LoginSignup } from '../pages/LoginSignup'
+import { Footer } from '../components/footer'
+import men_banner from '../assets/banner_mens.png'
+import women_banner from '../assets/banner_women.png'
+import kid_banner from '../assets/banner_kids.png'
 
 export function AppRoutes() {
     return (
@@ -14,9 +18,9 @@ export function AppRoutes() {
             <Routes>
                 <Route path='/' element={<Shop />} />
 
-                <Route path='/mens' element={<ShopCategory category="mens" />} />
-                <Route path='/womens' element={<ShopCategory category="womens" />} />
-                <Route path='/kids' element={<ShopCategory category="kids" />} />
+                <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+                <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+                <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
 
                 <Route path='/product' element={<Product />}>
                     <Route path=':productId' element={<Product />} />
@@ -26,6 +30,8 @@ export function AppRoutes() {
 
                 <Route path='/login' element={<LoginSignup />} />
             </Routes>
+
+            <Footer />
         </BrowserRouter>
     )
 }
