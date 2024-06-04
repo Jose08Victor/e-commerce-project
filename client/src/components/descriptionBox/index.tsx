bb2796fc6c99b496772ec8d3a43c5635c18b7c94
José Victor <jose08.victor@gmail.com>
2024-06-04 20:50:20 -0300

    feat: component descriptionBox added.

---
+++ client/src/components/descriptionBox/index.tsx
@@ -0,0 +1,18 @@
+import "./styles.css"
+
+export const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti, quidem veniam beatae ipsum fuga illum perferendis autem tempore soluta explicabo, ipsam esse, odio sunt possimus veritatis. Laboriosam, incidunt nisi! Soluta?Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quos hic vitae autem architecto itaque rem recusandae porro, atque temporibus facere ullam facilis animi fugiat veritatis ducimus mollitia possimus assumenda.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque consectetur ratione, adipisci dicta eius expedita tenetur officia tempora nobis quia. Praesentium, perferendis repellendus tempora quae culpa aspernatur velit ipsam laudantium.</p>
+            </div>
+        </div>
+    )
+}
