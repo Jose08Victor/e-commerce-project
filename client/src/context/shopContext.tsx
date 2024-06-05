e6657de24a6ff041157e4db49b64a46abb6cc261
José Victor <jose08.victor@gmail.com>
2024-06-05 20:43:23 -0300

    feat: added functions (addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems) and useState.

--- client/src/context/shopContext.tsx
+++ client/src/context/shopContext.tsx
@@ -1,14 +1,54 @@
-import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, createContext } from "react";
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, createContext, useState } from "react";
 import all_product from "../data/all_product";
 
 type ShopContextType = {
     all_product: typeof all_product;
-  };
+};
 
 export const ShopContext = createContext<ShopContextType>({ all_product });
 
+const getDefaultCart = () => {
+    let cart = {};
+    for (let i = 0; i < all_product.length + 1; i++) {
+        cart[i] = 0;
+    }
+    return cart;
+}
+
 export const ShopContextProvider = (props: { children: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => {
-    const contextValue = {all_product};
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = all_product.find((product) => product.id === Number(item));
+                totalAmount += cartItems[item] * itemInfo?.new_price;
+            }
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = { all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCartItems };
 
     return (
         <ShopContext.Provider value={contextValue}>
