592968c7fce420f63e44a9e86365ae3bdeb39aa0
José Victor <jose08.victor@gmail.com>
2024-06-10 18:33:03 -0300

    feat: page cart added.

---
+++ client/src/pages/cart/index.tsx
@@ -0,0 +1,10 @@
+import { CartItems } from "../../components/cartItems"
+import "./styles.css"
+
+export const Cart = () => {
+    return (
+        <div>
+            <CartItems/>
+        </div>
+    )
+}
