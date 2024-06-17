3bf57f1648234d5cf4ce771fd54b6fa211e4d4c0
José Victor <jose08.victor@gmail.com>
2024-06-17 18:39:40 -0300

    refactor: key added to the div.

--- client/src/components/cartItems/index.tsx
+++ client/src/components/cartItems/index.tsx
@@ -21,7 +21,7 @@ export const CartItems = () => {
 
             {all_product.map((e) => {
                 if (cartItems[e.id] > 0) {
-                    return <div>
+                    return <div key={e.id}>
                         <div className="cartitems-format cartitems-format-main">
                             <img src={e.image} alt="" className="carticon-product-icon" />
 
